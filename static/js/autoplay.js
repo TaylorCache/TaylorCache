c711f056c7143963d623aea3598fdc1f0e45f92c
明月偷垂泪 <12063904+the-moon-steals-tears@user.noreply.gitee.com>
2025-03-06 20:59:03 +0800

    Update README.md

---
+++ static/js/autoplay.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 1. 初始化 bulmaCarousel
+    var carousels = bulmaCarousel.attach('.carousel', {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        infinite: true
+    });
+
+    // 2. 自动播放下一组视频
+    function autoSwitch() {
+        setInterval(() => {
+            carousels[0].next(); // 轮播到下一组
+        }, 5000); // 每 5 秒自动切换
+    }
+
+    autoSwitch();
+
+    // 3. 给每个视频加上标题
+    document.querySelectorAll('.video-group .item video').forEach(video => {
+        let title = document.createElement("h3");
+        title.innerText = video.src.split('/').pop(); // 取文件名作为标题
+        title.style.textAlign = "center";
+        title.style.marginBottom = "5px";
+        video.parentNode.insertBefore(title, video);
+    });
+});
+
